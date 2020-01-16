9cd25a608cd6b583fc6126c5891db2f24854c3f2
Igor Gulamov <igor.gulamov@gmail.com>
2020-01-16 22:18:13 +0300

    update contracts

--- smart-contracts/contracts/ZeroPool.sol
+++ smart-contracts/contracts/ZeroPool.sol
@@ -14,21 +14,25 @@ contract ZeroPool is Ownable {
         bytes32 root_at;
         uint256[2] nullifier;
         uint256[2] utxo;
+        address owner;
         address token;
         uint256 delta;
         uint256[5][2] message;
         uint256[8] proof;
     }
 
-    struct Deposit {
-        address sender;
+    struct UTXO {
+        address owner;
         address token;
         uint256 amount;
-        bytes32 tx_hash;
     }
+
+
 
     mapping (bytes32=>uint32) public txs;
     mapping (bytes32=>bytes32) public deposits;
+    mapping (bytes32=>bytes32) public withdrawals;
+
     mapping (bytes32=>uint32) public deposit_consumed_at;
 
     uint32 public cursor;
@@ -38,10 +42,10 @@ contract ZeroPool is Ownable {
         return (deposit_consumed_at[tx_hash] > 0) && (deposit_consumed_at[tx_hash] < defect);
     }
 
-    function deposit(address token, uint256 _value, bytes32 tx_hash) public payable returns(bool) {
+    function deposit(address token, uint256 amount, bytes32 tx_hash) public payable returns(bool) {
         require(deposits[tx_hash]==bytes32(0));
-        require ((token != address(0)) || (msg.value == _value));
-        uint256 value = _value;
+        uint256 value = amount;
+        require ((token != address(0)) || (msg.value == value));
         if (asset != address(0)) {
             uint256 cur_balance = IERC20(token).balaceOf(this);
             IERC20(token).transferFrom(msg.sender, this, value);
@@ -49,29 +53,29 @@ contract ZeroPool is Ownable {
             require(value > 0);
         }
 
-        bytes32 deposit_hash = keccak256(abi.encode(Deposit(msg.sender, token, value, tx_hash), block.timestamp));
+        bytes32 deposit_hash = keccak256(abi.encode(msg.sender, token, value, block.timestamp));
         deposits[tx_hash] = deposit_hash;
         return true;
     }
 
-    function cancelDeposit(Deposit memory d, uint256 timestamp) public returns (bool) {
+    function cancelDeposit(UTXO memory d, uint256 timestamp, bytes32 tx_hash) public returns (bool) {
         require(timestamp < (block.timestamp + DEPOSIT_CANCEL_PERIOD));
-        require(!depositIsConsumed(d.tx_hash));
+        require(!depositIsConsumed(tx_hash));
 
         bytes32 deposit_hash = keccak256(abi.encode(d, timestamp));
-        require(deposits[d.tx_hash] == deposit_hash);
+        require(deposits[tx_hash] == deposit_hash);
         if (d.token == address(0)) {
-            payable(d.sender).transfer(d.amount);
+            payable(d.owner).transfer(d.amount);
         } else {
-            IERC20(d.token).transfer(d.sender, d.amount);
+            IERC20(d.token).transfer(d.owner, d.amount);
         }
 
-        delete deposits[d.tx_hash];
+        delete deposits[tx_hash];
         return true;
     }
 
 
-    function publishTx(Tx memory txn, bytes memory data) public onlyOwner returns(bool) {
+    function publishTx(Tx memory txn, uint256 deposit_timestamp) public onlyOwner returns(bool) {
         require((txs[txn.root_at] > 0) && (txs[txn.root_at] < cursor));
         bytes32 tx_hash = keccak256(abi.encode(txn, block.timestamp));
         require(txs[tx_hash] == 0);
@@ -79,13 +83,13 @@ contract ZeroPool is Ownable {
         if (txn.delta == 0) { //do nothing
 
         } else if (txn.delta < MAX_AMOUNT) { // deposit
-            require(deposits[tx_hash] == keccak256(data));
-            require(deposit_consumed_at[tx_hash]==0);
-            Deposit memory d = abi.decode(data, (Deposit));
-            require((d.token == txn.token) && (d.amount == txn.delta));
+            bytes32 deposit_hash = keccak256(abi.encode(txn.owner, txn.token, txn.delta, deposit_timestamp));
+            require(deposits[tx_hash] == deposit_hash);
+            require(deposit_consumed_at[tx_hash] == 0);
+            deposit_consumed_at[tx_hash] = cursor;
  
-        } else if (delta > (BN254_ORDER-MAX_AMOUNT)) { //withdrawal
-        // TODO withdrawal
+        } else if (txn.delta > (BN254_ORDER-MAX_AMOUNT)) { //withdrawal
+            withdrawals[tx_hash] = keccak256(abi.encode(txn.owner, txn.token, BN254_ORDER-txn.delta, block.timestamp));
 
         } else revert();
 
