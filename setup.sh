c6a53b985e87bd16cd3f723401750e1112c97d99
artemvorobev <artall64@gmail.com>
2020-01-17 22:18:36 +0200

    Display errors that could appear in a sh scripts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
+set -e
+set -o pipefail
+
 mkdir -p circuitsCompiled
 npx circom ./circuits/transaction.circom -o ./circuitsCompiled/transaction.json
-npx snarkjs setup -c ./circuitsCompiled/transaction.json --pk ./circuitsCompiled/transaction_pk.json --vk circuitsCompiled/transaction_vk.json --protocol groth
-node ./node_modules/websnark/tools/buildpkey.js -i circuitsCompiled/transaction_pk.json -o circuitsCompiled/transaction_pk.bin
+npx snarkjs setup -c ./circuitsCompiled/transaction.json --pk ./circuitsCompiled/transaction_pk.json --vk ./circuitsCompiled/transaction_vk.json --protocol groth
+node ./node_modules/websnark/tools/buildpkey.js -i ./circuitsCompiled/transaction_pk.json -o ./circuitsCompiled/transaction_pk.bin
