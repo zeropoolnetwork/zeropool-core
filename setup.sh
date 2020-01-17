56d768ad8fe2159fd278e1929dd297b7ba6b9048
artemvorobev <artall64@gmail.com>
2020-01-17 23:33:32 +0200

    npm i + build:browser

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@
 set -e
 set -o pipefail
 
+npm i
+
 mkdir -p circuitsCompiled
 npx circom ./circuits/transaction.circom -o ./circuitsCompiled/transaction.json
 npx snarkjs setup -c ./circuitsCompiled/transaction.json --pk ./circuitsCompiled/transaction_pk.json --vk ./circuitsCompiled/transaction_vk.json --protocol groth
 node ./node_modules/websnark/tools/buildpkey.js -i ./circuitsCompiled/transaction_pk.json -o ./circuitsCompiled/transaction_pk.bin
+
+npm run build:browser
