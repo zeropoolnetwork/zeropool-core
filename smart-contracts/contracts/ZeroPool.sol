4cac5cca95e89c7837d25cc940714c1236962bb3
Igor Gulamov <igor.gulamov@gmail.com>
2020-01-20 19:49:13 +0300

    contract update

--- smart-contracts/contracts/ZeroPool.sol
+++ smart-contracts/contracts/ZeroPool.sol
@@ -42,12 +42,18 @@ contract Zeropool is Ownable{
         IERC20 token;
         uint256 delta;
         TxExternalFields ext;
+        uint256[8] proof;
     }
 
     struct BlockItem {
         Tx ctx;
         uint256 new_root;
         uint256 deposit_blocknumber;
+    }
+    struct BlockItemNote {
+        bytes32[8] proof;
+        uint256 id;
+        BlockItem item;
     }
 
     struct UTXO {
@@ -73,6 +79,16 @@ contract Zeropool is Ownable{
     VK tx_vk;
     VK tree_update_vk;
     
+
+    function blokItemNoteVerify(BlockItemNote memory note) internal view returns(bool) {
+        (bytes32 itemhash, bytes32) = blockItemHash(note.item);
+        return MerkleProof.keccak256MerkleProof(note.proof, note.id & 0xff, itemhash) == rollup_block[note.id>>8];
+    }
+
+    function blockItemHash(BlockItem memory item) internal view returns(bytes32 itemhash, bytes32 txhash) {
+        txhash = keccak256(abi.encode(item.ctx));
+        itemhash = keccak256(abi.encode(txhash, item.new_root, item.deposit_blocknumber));
+    }
 
     function groth16verify(VK memory vk, Proof memory proof, uint256[] memory inputs) internal view returns(bool) {
         // TODO bind groth16verifier
@@ -119,7 +135,8 @@ contract Zeropool is Ownable{
         bytes32[] memory hashes = new bytes32[](nitems);
         for(uint i = 0; i<nitems; i++) {
             BlockItem memory item = items[i];
-            bytes32 txhash = keccak256(abi.encode(item));
+            bytes32 itemhash = keccak256(abi.encode(item));
+            bytes32 txhash = keccak256(abi.encode(item.ctx));
             if (item.ctx.delta == 0) {
                 require(item.deposit_blocknumber == 0);
                 require(item.ctx.token == IERC20(0));
@@ -135,23 +152,57 @@ contract Zeropool is Ownable{
                 withdraw_state[withdraw_hash] = rollup_tx_num + i;
             } else revert();
 
-            hashes[i] = txhash;
+            hashes[i] = itemhash;
         }
         rollup_block[rollup_tx_num >> 8] = MerkleProof.keccak256MerkleTree(hashes);
         rollup_tx_num += 256;
         return true;
     }
 
-    function challengeTxProof() public {
-
+    function stopRollup() public {
+        //TODO implement rollup stop logic
     }
 
-    function challengeUTXOTreeUpdate() public {
-
+    function challengeTx(BlockItemNote memory cur, BlockItemNote memory base) public returns(true) {
+        require(blockItemNoteVerify(cur) && blockItemNoteVerify(base));
+        require(cur.item.ctx.rootptr == base.id);
+        uint256[] memory inputs = new uint256[](8);
+        inputs[0] = base.item.new_root;
+        inputs[1] = cur.item.ctx.nullifier[0];
+        inputs[2] = cur.item.ctx.nullifier[1];
+        inputs[3] = cur.item.ctx.utxo[0];
+        inputs[4] = cur.item.ctx.utxo[1];
+        inputs[5] = uint256(cur.item.ctx.token);
+        inputs[6] = cur.item.ctx.delta;
+        inputs[7] = uint256(keccak256(abi.encode(cur.item.ctx.ext)));
+        require(!groth16verify(tx_vk, cur.item.ctx.proof, inputs) || cur.item.ctx.rootptr >= cur.id);
+        stopRollup();
+        return true;
     }
 
-    function challengeDoubleSpend() public {
+    function challengeUTXOTreeUpdate(BlockItemNote memory cur, BlockItemNote memory prev) public {
+        require(blockItemNoteVerify(cur) && blockItemNoteVerify(prev));
+        require(cur.id == prev.id+1);
+        uint256[] memory inputes = new uint256[](5);
+        inputs[0] = prev.new_root;
+        inputs[1] = cur.new_root;
+        inputs[2] = prev.id;
+        inputs[3] = cur.utxo[0];
+        inputs[4] = cur.utxo[1];
+        require(!groth16verify(tree_update_vk, cur.item.ctx.proof, inputs));
+        stopRollup();
+        return true;
+    }
 
+    function challengeDoubleSpend(BlockItemNote memory cur, BlockItemNote memory prev) public {
+        require(blockItemNoteVerify(cur) && blockItemNoteVerify(prev));
+        require(cur.id > prev.id);
+        require(cur.item.ctx.nullifier[0] == prev.item.ctx.nullifier[0] ||
+            cur.item.ctx.nullifier[0] == prev.item.ctx.nullifier[1] ||
+            cur.item.ctx.nullifier[1] == prev.item.ctx.nullifier[0] ||
+            cur.item.ctx.nullifier[1] == prev.item.ctx.nullifier[1]);
+        stopRollup();
+        return true;
     }
 
 
--- smart-contracts/contracts/lib/MerkleProof.sol
+++ smart-contracts/contracts/lib/MerkleProof.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.6.0;
 
 library MerkleProof {
-    function keccak256MerkleProof(bytes32[8] memory proof, uint8 path, bytes32 leaf) internal pure returns(bytes32) {
+    function keccak256MerkleProof(bytes32[8] memory proof, uint path, bytes32 leaf) internal pure returns(bytes32) {
         bytes32 root = leaf;
-        for(uint8 i=0; i<8; i++) {
+        for(uint i = 0; i < 8; i++) {
             root = path >> i & 1 == 0 ?  keccak256(abi.encode(leaf, proof[i])) : keccak256(abi.encode(proof[i], leaf));
         }
         return root;
