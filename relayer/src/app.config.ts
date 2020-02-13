77944fce2fa5914b35cc0d9b9bee94618789f672
artemvorobev <artall64@gmail.com>
2020-02-13 16:17:54 -0700

    Use config files to start

--- relayer/src/app.config.ts
+++ relayer/src/app.config.ts
@@ -1,29 +1,23 @@
-// import * as config from 'config';
+console.log( process.env.NODE_ENV)
 
-// const appConfig = config.get('app'); // todo: describe it
-// const networkConfig = config.get('network');
+import * as config from 'config';
+
+
+const appConfig = config.get('app') as any;
+const networkConfig = config.get('network') as any;
 
 export const AppConfig = {
-  port: +process.env.PORT || 3000,
-  txAggregationTime: +process.env.BLOCK_TIME || 2000,
+    port: process.env.PORT || appConfig.port || 3000,
+    txAggregationTime: process.env.BLOCK_TIME || appConfig.txAggregationTime || 2000,
 };
 
 export const NetworkConfig = {
-  rpc: process.env.RPC || 'http://127.0.0.1:8545',
-  contract: process.env.CONTRACT_ADDRESS,
-  etherscan_prefix: process.env.ETHERSCAN_PREFIX
+    rpc: process.env.RPC || networkConfig.rpc,
+    contract: process.env.CONTRACT_ADDRESS || networkConfig.contract,
+    etherscan_prefix: process.env.ETHERSCAN_PREFIX || networkConfig.etherscan_prefix,
 };
 
-// export const AppConfig = {
-//     port: process.env.PORT || appConfig.port || 3000,
-//     txAggregationTime: process.env.BLOCK_TIME || appConfig.txAggregationTime || 2000,
-// };
-//
-// export const NetworkConfig = {
-//     rpc: process.env.RPC || networkConfig.rpc || 'http://127.0.0.1:8545',
-//     contract: process.env.CONTRACT_ADDRESS || networkConfig.contract,
-//     etherscan_prefix: process.env.ETHERSCAN_PREFIX || networkConfig.etherscan_prefix,
-// };
-
-export const Mnemonic = process.env.MNEMONIC;
-// export const Mnemonic = process.env.MNEMONIC || config.get('mnemonic');
+console.log('Mnemonic: #1');
+export const Mnemonic = process.env.MNEMONIC || config.get('mnemonic');
+console.log(Mnemonic);
+console.log('Mnemonic: #2');
