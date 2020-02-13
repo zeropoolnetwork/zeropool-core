d914425d75ebd934094a46b04bd782791c1b8a85
artemvorobev <artall64@gmail.com>
2020-02-13 11:14:44 -0700

    Added relayer call with axios

--- zp-cli/src/commands/deposit.ts
+++ zp-cli/src/commands/deposit.ts
@@ -3,7 +3,10 @@ import * as ethUtils from '../../../lib/ethereum/ethereum';
 import * as ZeroPoolNetwork from '../../../lib/zero-pool-network';
 import Base from '../base';
 import { HdWallet, DomainEthereum } from '@buttonwallet/blockchain-ts-wallet-core';
+const axios = require('axios').default;
 
+
+const ETH_ASSET_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 export default class Deposit extends Base {
   static description = 'Show ZeroPool tx history'
@@ -19,11 +22,16 @@ TODO: put example of response
 
     cli.action.start(`Prepare deposit transaction ${this.amount} ${this.asset}`)
     const blockItemObj = await this.makeDeposit();
-    // TODO: log tx object
+
     cli.action.start(`Send transaction to relayer ${this.relayerEndpoint}`)
-    // TODO: put actual relayer call in place
+
+    await axios.post(`${this.relayerEndpoint}/tx`, blockItemObj)
+    //
+    // TODO: some print in a console
     // const blockItemObj2 = await this.makeDeposit();
     // publishBlockItems
+
+    // TODO: return eth transaction hash from relayer if possible
     // cli.url('https://etherscan.io/tx/0x3fd80cffa3c06ff693d8685e8feb3526fb23ad7caa62186d46e718492351fcf3', 'https://etherscan.io/tx/0x3fd80cffa3c06ff693d8685e8feb3526fb23ad7caa62186d46e718492351fcf3')
 
     cli.action.stop()
@@ -33,20 +41,21 @@ TODO: put example of response
     process.exit();
   }
 
-  async makeDeposit(): Promise<any> {
+  makeDeposit(): Promise<any> {
+    // TODO: move to base class
     const wallet = new HdWallet(this.mnemonic, '');
     const eth = wallet.generateKeyPair(DomainEthereum.Instance(), 0);
     const zp = new ZeroPoolNetwork(this.contractAddress, eth.privateKey, this.mnemonic, this.rpcEndpoint);
 
+    // TODO: move to base class
     const assetAddress = this.asset === 'ETH'
-      // TODO: Define constant
-      ? '0x0000000000000000000000000000000000000000'
-      // TODO: In case of main-net (by endpoint or flag) resolve 'DAI' into addresses
-      : this.asset
+      ? ETH_ASSET_ADDRESS
+      : this.asset // TODO: In case of main-net (by endpoint or flag) resolve 'DAI' into addresses
 
+    // TODO: move to base class
     const amountOfAsset = ethUtils.tw(this.amount);
 
-    return await zp.deposit(assetAddress, amountOfAsset);
+    return zp.deposit(assetAddress, amountOfAsset);
   }
 
 
