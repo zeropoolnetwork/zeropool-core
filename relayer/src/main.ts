6cfe9c36f7e2a5e115b694d6989734c116bd0637
artemvorobev <artall64@gmail.com>
2020-03-03 15:34:34 +0200

    Lunch as microservice

--- relayer/src/main.ts
+++ relayer/src/main.ts
@@ -4,19 +4,19 @@ import { AppModule } from './app.module';
 import { AppConfig } from "./app.config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.createMicroservice(AppModule);
 
-  const options = new DocumentBuilder()
-    .setTitle('ZeroPool Relayer')
-    .setDescription('Relayer accepts requests for publishing transactions')
-    .setVersion('1.0')
-    .addTag('RelayerAPI')
-    .build();
-
-  // app.use(json({ limit: '50mb' })); // json limit
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  // const options = new DocumentBuilder()
+  //   .setTitle('ZeroPool Relayer')
+  //   .setDescription('Relayer accepts requests for publishing transactions')
+  //   .setVersion('1.0')
+  //   .addTag('RelayerAPI')
+  //   .build();
+  //
+  // // app.use(json({ limit: '50mb' })); // json limit
+  //
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('docs', app, document);
 
   await app.listen(AppConfig.port);
 }
